eb9f5af2230638278cabac2a6daf81481927b467
Chris Turner <chris@forloop.space>
2020-03-15 10:15:26 +0000

    wip

--- src/main/java/io/terrible/task/processor/controller/TaskController.java
+++ src/main/java/io/terrible/task/processor/controller/TaskController.java
@@ -4,6 +4,7 @@ package io.terrible.task.processor.controller;
 import io.terrible.task.processor.services.WorkerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,35 +17,36 @@ import reactor.core.publisher.Mono;
  * @author Chris Turner (chris@forloop.space)
  */
 @Slf4j
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class TaskController {
 
-  private final WorkerService workerService;
+    private final WorkerService workerService;
 
-  /**
-   * Generate the thumbnails for a given video file.
-   *
-   * @param path where the video file is located
-   */
-  @GetMapping("/task/thumbnails")
-  public Mono<Boolean> thumbnails(@RequestParam final String path) {
+    /**
+     * Generate the thumbnails for a given video file.
+     *
+     * @param path where the video file is located
+     */
+    @GetMapping("/task/thumbnails")
+    public Mono<Boolean> thumbnails(@RequestParam final String path) {
 
-    final boolean messageSent = workerService.createThumbnail(path);
+        final boolean messageSent = workerService.createThumbnail(path);
 
-    return Mono.justOrEmpty(messageSent);
-  }
+        return Mono.justOrEmpty(messageSent);
+    }
 
-  /**
-   * Scan a directory looking for videos
-   *
-   * @param path for where a recursive scan should start
-   */
-  @GetMapping("/task/directories")
-  public Mono<Boolean> directories(@RequestParam final String path) {
+    /**
+     * Scan a directory looking for videos
+     *
+     * @param path for where a recursive scan should start
+     */
+    @GetMapping("/task/directories")
+    public Mono<Boolean> directories(@RequestParam final String path) {
 
-    final boolean messageSent = workerService.scanDirectory(path);
+        final boolean messageSent = workerService.scanDirectory(path);
 
-    return Mono.justOrEmpty(messageSent);
-  }
+        return Mono.justOrEmpty(messageSent);
+    }
 }
